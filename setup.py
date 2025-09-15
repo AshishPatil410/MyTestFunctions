7d4cc880ab29d61269351d61e661a0a99a6c04a4
AshishPatilOutbound <ashish.patil@outbound.ai>
2025-09-15 15:58:43 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+install_requires = [
+]
+
+setup(
+    name='my-func-lib',
+    version='1.0.0',
+    author='ashish',
+    author_email='ashishpatil2@icloud.com',
+    description='This package contains test functions to learn pytest',
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    include_package_data=True,
+    classifiers=[
+        "Framework :: Pytest",
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+)
+
